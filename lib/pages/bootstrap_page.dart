65fa369a372a8209d2fd9c77527e7d6daad28c01
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-12 14:36:59 -0600

    Resume pending Task Book review on launch

--- lib/pages/bootstrap_page.dart
+++ lib/pages/bootstrap_page.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 import 'package:firepath/nav.dart';
+import 'package:firepath/services/task_book_setup_store.dart';
 import 'package:firepath/state/app_state.dart';
 
 class BootstrapPage extends StatefulWidget {
@@ -21,11 +22,19 @@ class _BootstrapPageState extends State<BootstrapPage> {
     final state = context.watch<AppState>();
     if (_navigated || !state.bootstrapped) return;
     _navigated = true;
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) return;
-      final target = state.onboardingComplete ? AppRoutes.home : AppRoutes.onboarding;
-      context.go(target);
-    });
+    WidgetsBinding.instance.addPostFrameCallback((_) => _route(state));
+  }
+
+  Future<void> _route(AppState state) async {
+    if (!mounted) return;
+    if (!state.onboardingComplete) {
+      context.go(AppRoutes.onboarding);
+      return;
+    }
+
+    final reviewPending = await TaskBookSetupStore().isReviewPending();
+    if (!mounted) return;
+    context.go(reviewPending ? AppRoutes.taskBookReview : AppRoutes.home);
   }
 
   @override
